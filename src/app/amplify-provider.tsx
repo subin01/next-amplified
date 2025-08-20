ec94d0a54fbf671b6b92d6c814fbfc4ca28f50da
Subin Paul <subin01@gmail.com>
2025-08-20 16:48:20 +1000

    path err fix

--- src/app/amplify-provider.tsx
+++ src/app/amplify-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Amplify } from 'aws-amplify';
-import config from '@/amplifyconfiguration.json';
+import config from '../../amplifyconfiguration.json';
 
 Amplify.configure(config);
 
